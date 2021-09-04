c4bd86dab0dc57314c50260015588161f63f70fb
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2021-09-04 00:37:27 -0300

    Styled the aplication

--- note-app/src/components/UpdateTodo.jsx
+++ note-app/src/components/UpdateTodo.jsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react'
 import {MdClose} from "react-icons/md";
 
+import "../styles/UpdateTodo/UpdateTodo.css"
+
 function UpdateTodo({onClose, text, onUpdate}) {
     const [input, setInput] = useState('')   
     
     return (
-        <div>
-            <button onClick={onClose}><MdClose/></button>
+        <div className="layout">
+            <button className="update-button" onClick={onClose}> <MdClose size="25px"/> </button>
             <form >
                 <input type="text" value={input} placeholder={text} onChange={(e) => setInput(e.target.value)}/>
                 <button onClick={(e) => {
